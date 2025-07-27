3b8dd825ae6bbb8f97286056d392057eb9077698
Peter Szarvas <contact@peterszarvas.hu>
2025-07-27 11:15:45 +0200

    fix: tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,11 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  def sign_in_as(user)
+    visit new_session_url
+    fill_in "Enter your email address", with: user.email_address
+    fill_in "Enter your password", with: "password"
+    click_on "Sign in"
+  end
 end
